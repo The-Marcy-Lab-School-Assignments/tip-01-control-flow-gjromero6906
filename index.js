babc59752144b380f8767cfb852a822399e0cf0e
gjromero6906 <gjr6906@gmail.com>
2025-09-30 12:45:24 -0400

    testing and created outfit picker

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ Below 40 → "Wear a heavy coat, scarf, and boots."
 
 const outfitPicker = (temp) => {
   // ✍️ Write your code here
+  if(temp<40){
+    console.log("Wear a heavy coat, scarf, and boots.");
+  }else if(temp<60){
+    console.log("Wear a sweater and pants.");
+  }else if(temp<80){
+    console.log("Wear jeans and a light jacket.");
+  }else{
+    console.log("Wear shorts and a t-shirt.");
+  }
 };
 
 //Test your function below
+outfitPicker(80);
+outfitPicker(39);
+outfitPicker(40);
